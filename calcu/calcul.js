4eeb6e4877747c0bf4e627c5f026f84dddf8aa46
unknown <sivanesansivanesan988@gmail.com>
2026-06-15 10:58:12 +0530

    task finish

---
+++ calcu/calcul.js
@@ -0,0 +1,5 @@
+const display=document.getElementById("display");
+const operands=['+','-','*','/','%']
+function append(value){
+    display.value += value;
+}
---
+++ javascript/even.js
@@ -0,0 +1,6 @@
+function add(){
+    const a=document.getElementById(s).value;
+    const b=document.getElementById(v).value;
+    let c=(number(a))+(number(b));
+    console.log(c)
+}
