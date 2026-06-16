c5c03081cea55e947cc011745befa0bd18ac4ca6
unknown <sivanesansivanesan988@gmail.com>
2026-06-16 11:15:06 +0530

    JS

---
+++ week2/first class/EDA.js
@@ -0,0 +1,41 @@
+console.log("A")
+console.log("B")
+console.log("C")
+
+// const promise=new Promise((resolve, reject)=>{
+    // const success=true;
+    // if(success){
+    //     console.log("Done")
+    // }
+    // else{
+    //     console.log("No")
+    // }
+
+//     setTimeout(()=>{
+//         console.log("Hi")
+//     },2000)
+//     setTimeout(()=>{
+//         console.log("Done")
+//     },0)
+//     console.log("D")
+// })
+// promise.then(res=>{console.log(res)})
+// .catch(err=>{console.log(err)})
+
+console.log(1)
+console.log(2)
+console.log("Hi")
+
+function example(){                           //function
+    console.log("Hi veera")
+}
+const example1 = ()=>{    
+    let i=50;
+    let j=50;                  //auro function
+    console.log(i+j);
+}
+
+setTimeout(example,3000)
+setTimeout(example1,300)
+
+
